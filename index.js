7a6e5ff4246fa30d1e2e1a19957979892d6782d5
Sakariyah Abdulhazeem <sakariyahabdulhazeem@gmail.com>
2024-05-24 14:07:50 +0100

    code update

--- index.js
+++ index.js
@@ -67,3 +67,24 @@ function Mouseeeee() {
   });  
 }
 Mouseeeee()
+
+
+
+
+
+// faq code 
+
+document.addEventListener("DOMContentLoaded", function () {
+  var acc = document.getElementsByClassName("accordion-item");
+  for (var i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
+});
